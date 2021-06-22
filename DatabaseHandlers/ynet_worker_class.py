ed09b380945844a0f59e9eb32447e1da18c3ee71
Yoav <yoav.meron11@gmail.com>
2021-06-22 13:02:12 +0300

    created ynet worker class that parses and returns the text of the article

---
+++ DatabaseHandlers/ynet_worker_class.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+from basic_worker import BasicWorker
+
+
+class YnetWorker(BasicWorker):
+    def __init__(self, url):
+        super().__init__(url)
+        self.page_html = self.download_link()
+
+    def parse(self):
+        soup = BeautifulSoup(self.page_html, "html.parser")
+        texts = []
+        texts_span = soup.find_all("span", {"data-text": "true"})
+        for span in texts_span:
+            texts.append(span.getText())
+        texts.pop(-1)
+        full_text = ''.join(texts)
+        return full_text
+
+
+worker = YnetWorker("https://www.ynet.co.il/news/article/ryFma1113u#autoplay")
+worker.parse()
