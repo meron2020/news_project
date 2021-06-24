801563812b310fbbded1b977c9dc134243326c9a
Yoav <yoav.meron11@gmail.com>
2021-06-24 15:18:29 +0300

    deleted an unnecessary urllib method

--- News Crawlers/basic_crawler.py
+++ News Crawlers/basic_crawler.py
@@ -15,19 +15,9 @@ class BasicCrawler:
         self.channel.queue_declare(queue='news urls', durable=True)
 
     @classmethod
-    def urllib_get_link(cls, url):
-        base_url = Request(url, headers={
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36'}
-                           )
-        page = urlopen(base_url)
-        page_html = page.read().decode('utf-8')
-        return page_html
-
-    @classmethod
     def get_link(cls, url):
         r = requests.get(url)
         return r.text
-
 
     def send_links_to_queue(self, url_links):
         for link in url_links:
