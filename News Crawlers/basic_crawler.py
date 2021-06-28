7c44a48434110f8ca20bcfb794a12cfb293523a8
Yoav <yoav.meron11@gmail.com>
2021-06-28 12:23:30 +0300

    updated basic crawler to have no interaction with rabbitmq as all interaction will be managed by CrawlersHandler.py

--- News Crawlers/basic_crawler.py
+++ News Crawlers/basic_crawler.py
@@ -1,32 +1,15 @@
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
 import requests
-import pika
 
 
 class BasicCrawler:
     def __init__(self, url):
         self.root_links = []
         self.page_html = self.get_link(url)
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters('localhost')
-        )
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='news urls', durable=True)
 
     @classmethod
     def get_link(cls, url):
         r = requests.get(url)
         return r.text
-
-    def send_links_to_queue(self, url_links):
-        for link in url_links:
-            self.channel.basic_publish(
-                exchange='',
-                routing_key='news urls',
-                body=link,
-                properties=pika.BasicProperties(delivery_mode=2)
-            )
 
     def check_if_links_change(self, find_news_links):
         news_links = find_news_links()
