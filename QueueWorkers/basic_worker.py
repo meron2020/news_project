dec0bc981ee0d8482fbc10153f67babc1aea7bc2
Yoav <yoav.meron11@gmail.com>
2021-06-28 12:29:48 +0300

    added acknowledgement print function

--- QueueWorkers/basic_worker.py
+++ QueueWorkers/basic_worker.py
@@ -6,7 +6,12 @@ import pika
 class BasicWorker:
     def __init__(self, url):
         self.url = url
+        self.page_html = self.download_link()
 
     def download_link(self):
         r = requests.get(self.url)
         return r.text
+
+    @classmethod
+    def print_acknowledgement(cls, newspapaer):
+        print("{} url received.".format(newspapaer))
