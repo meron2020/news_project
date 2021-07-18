e90964a8b04d7d78b998455d479d6caed8e6f904
Yoav <yoav.meron11@gmail.com>
2021-07-18 11:27:25 +0300

    Added topic to url

--- News Crawlers/maariv_crawler.py
+++ News Crawlers/maariv_crawler.py
@@ -3,17 +3,24 @@ from basic_crawler import BasicCrawler
 
 
 class MaarivCrawler(BasicCrawler):
-    def __init__(self, connection_parameter):
+    def __init__(self):
         super(MaarivCrawler, self).__init__("https://www.maariv.co.il/news")
-        self.news_links = []
+        self.no_topic_news_links = []
         self.soup = BeautifulSoup(self.page_html, "html.parser")
+        self.topic_dict = {"politics": 'המערכת הפוליטית', "military": 'צבא וביטחון', "world": 'חדשות בעולם',
+                           "health": 'בריאות',
+                           "law": 'משפט ופלילים', "Education": 'חינוך', "israel": 'כללי'}
+
+        self.news_links = []
 
     def find_big_item_links(self):
         news_links = []
         big_items_list = self.soup.find_all("div", {"class": "category-five-articles-big-item"})
-        for big_item in big_items_list:
-            news_link = big_item.a['href']
+
+        for i in range(len(big_items_list)):
+            news_link = big_items_list[i].a['href']
             news_links.append(news_link)
+
         return news_links
 
     def find_small_item_links(self):
@@ -27,7 +34,22 @@ class MaarivCrawler(BasicCrawler):
     def return_news_links(self):
         big_links = self.find_big_item_links()
         small_links = self.find_small_item_links()
-        self.news_links.extend(big_links)
-        self.news_links.extend(small_links)
+        self.no_topic_news_links.extend(big_links)
+        self.no_topic_news_links.extend(small_links)
 
+        for link in self.no_topic_news_links:
+            for topic_key in self.topic_dict.keys():
+                if topic_key in link:
+                    if topic_key == "Education" or topic_key == "health":
+                        self.news_links.append([link, 'חינוך ובריאות'])
+                    else:
+                        self.news_links.append([link, self.topic_dict[topic_key]])
+
+        # print(self.news_links)
+        # print(len(self.news_links))
         return self.news_links
+
+
+# crawler = MaarivCrawler()
+# # crawler.find_big_item_links()
+# crawler.return_news_links()
