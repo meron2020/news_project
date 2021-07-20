26b295dc24a0be257b0011cddb30383d606ceaad
Yoav <yoav.meron11@gmail.com>
2021-07-20 23:41:49 +0300

    Instantiates and handles the database handler.

---
+++ DatabaseHandlers/database_handler_orchestrator.py
@@ -0,0 +1,17 @@
+from DatabaseHandlers.database_handler import DatabaseHandler
+import sqlite3
+
+
+class DatabaseHandlerOrchestrator:
+    @classmethod
+    def run_orchestrator(cls):
+        connection = sqlite3.connect("news_texts.db")
+        cursor = connection.cursor()
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, newspaper TEXT,"
+            "full_text TEXT, topic Text,cluster_id Text);")
+        print("Successfully Connected to SQLite")
+
+        handler = DatabaseHandler(str(0), connection, cursor)
+
+        handler.start_consumption()
