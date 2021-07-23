7a30e389969191f550b6a8d774363a9a9c75e22c
Yoav <yoav.meron11@gmail.com>
2021-07-23 22:34:49 +0300

    Stopped usage of israel hayom for project

--- Parsers/israel_hayom_parser.py
+++ Parsers/israel_hayom_parser.py
@@ -8,5 +8,5 @@ class IsraelHayomWorker(BasicWorker):
 
     def parse(self):
         soup = BeautifulSoup(self.page_html, "html.parser")
-        text_div = soup.body.find("div", {"class": "text-content"})
+        text_div = soup.find("div", {"class": "text-content"})
         return text_div.get_text()
