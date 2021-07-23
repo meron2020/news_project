50ae144db051547149447efe506eecfa5f4b581e
Yoav <yoav.meron11@gmail.com>
2021-07-23 22:34:03 +0300

    Created Walla Crawler

--- News_Crawlers/walla_crawler.py
+++ News_Crawlers/walla_crawler.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from basic_crawler import BasicCrawler
-from datetime import datetime
 
 
 class WallaCrawler(BasicCrawler):
@@ -14,36 +13,22 @@ class WallaCrawler(BasicCrawler):
                            "https://news.walla.co.il/category/2"]
 
         self.news_links = []
-        # self.find_current_news_links()
-        # self.send_links_to_queue(self.news_links)
 
-    # def find_current_news_links(self):
-    #     for link in self.root_links:
-    #         html = requests.get(link).text
-    #         soup = BeautifulSoup(html, 'html.parser')
-    #         current_news_links = soup.find("section", {"class": "category-content"}).ul
-    #         for current_link in current_news_links:
-    #             pub_date = current_link.div[{'class': 'pub-date'}]
-    #             date_today = datetime.today().strftime('%Y-%m-%d')
-    #             if date_today[-2:] == pub_date[:2]:
-    #                 try:
-    #                     news_link = current_link.a['href']
-    #                     self.news_links.append(news_link)
-    #                 except Exception:
-    #                     pass
-
-    def new_finding_method(self):
+    def find_all_links(self):
         # for link in self.root_links:
-        html = requests.get("https://news.walla.co.il/category/2689").text
+        html = requests.get("https://news.walla.co.il/").text
         soup = BeautifulSoup(html, 'html.parser')
-        all_links = soup.body.find_all("a")
-        for link in all_links:
-            try:
-                if 'item' in link['href']:
-                    print(link['href'])
-            except Exception:
-                pass
+        events_divs = soup.body.find_all("div", {"class": 'events'})
+        all_links = []
+        events_divs.pop(2)
+        events_divs.pop(-1)
+        events_divs.pop(-2)
+        for div in events_divs:
+            for link in div.find_all("a"):
+                all_links.append(link['href'])
+
+        print(len(all_links))
 
 
 crawler = WallaCrawler()
-crawler.new_finding_method()
+crawler.find_all_links()
