98ab176e9ce28edf7196c700188e20b81c2f394b
Yoav <yoav.meron11@gmail.com>
2021-07-23 13:21:03 +0300

    Finished Ynet part of test

--- Mini_NLP/mini_nlp_test.py
+++ Mini_NLP/mini_nlp_test.py
@@ -20,24 +20,37 @@ def create_db():
 
 handler = create_db()
 
-ynet_urls_dict = {"https://www.ynet.co.il/news/article/sju500x4cu#autoplay": 'צבא וביטחון',
-                  "https://www.ynet.co.il/news/article/skfpuovcu#autoplay": 'מדיני',
-                  "https://www.ynet.co.il/news/article/s1arbdsru#autoplay": 'חדשות בעולם',
-                  "https://www.ynet.co.il/news/article/rjj00t4ea00": 'חדשות בעולם',
-                  "https://www.ynet.co.il/news/article/s1vo1i4ru#autoplay": 'צבא וביטחון'}
+urls_dict = {"https://www.ynet.co.il/news/article/sju500x4cu#autoplay": 'צבא וביטחון',
+             "https://www.ynet.co.il/news/article/skfpuovcu#autoplay": 'מדיני',
+             "https://www.ynet.co.il/news/article/s1arbdsru#autoplay": 'חדשות בעולם',
+             "https://www.ynet.co.il/news/article/rjj00t4ea00": 'חדשות בעולם',
+             "https://www.ynet.co.il/news/article/s1vo1i4ru#autoplay": 'צבא וביטחון',
+             }
 
-for url in ynet_urls_dict.keys():
-    ynet_urls_list = list(ynet_urls_dict.keys())
-    number_list = range(1, 5)
-    cluster_id = ""
+for url in urls_dict.keys():
+    ynet_urls_list = list(urls_dict.keys())
+    number_list = list(range(1, 5))
+    cluster_ids = []
     for i in range(3):
-        cluster_id += str(random.choice(number_list))
-        cluster_id += " "
+        cluster_ids.append(str(random.choice(number_list)))
+        number_list.remove(int(cluster_ids[-1]))
+    cluster_ids_str = ",".join(cluster_ids)
 
-    worker = YnetWorker(url)
+    if "ynet" in url:
+        worker = YnetWorker(url)
+        newspaper = "ynet"
+    elif "maariv" in url:
+        worker = MaarivWorker(url)
+        newspaper = "maariv"
+    elif "mako" in url:
+        worker = N12Worker(url)
+        newspaper = "mako"
+    else:
+        worker = IsraelHayomWorker(url)
+        newspaper = "israel hayom"
+
     full_text = worker.parse()
-    handler.insert_article("ynet", url, full_text, ynet_urls_dict[url])
-    handler.update_cluster_id(ynet_urls_list.index(url) + 1, cluster_id)
+    full_text = full_text.replace("'", "")
 
-
-
+    handler.insert_article(newspaper, url, full_text, urls_dict[url])
+    handler.update_cluster_id(ynet_urls_list.index(url) + 1, cluster_ids_str)
