4d7a785f977f603dbdab8d3d0ae51430aa2a9aa2
Yoav <yoav.meron11@gmail.com>
2021-07-26 14:40:12 +0300

    deleted rows

--- DatabaseHandlers/database_handler_orchestrator.py
+++ DatabaseHandlers/database_handler_orchestrator.py
@@ -3,16 +3,17 @@ import sqlite3
 
 
 class DatabaseHandlerOrchestrator:
-    @classmethod
-    def run_orchestrator(cls):
+
+    def run_orchestrator(self):
         connection = sqlite3.connect("news_texts.db")
         cursor = connection.cursor()
         table_name = "articles"
+        handler = DatabaseHandler(str(0), connection, cursor, table_name)
+        print("Successfully Connected to SQLite")
+        handler.delete_all_rows()
+        print("Successfully deleted all rows")
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, newspaper TEXT,"
             "full_text TEXT, topic Text,cluster_id Text);")
-        print("Successfully Connected to SQLite")
-
-        handler = DatabaseHandler(str(0), connection, cursor, table_name)
 
         handler.start_consumption()
