bfd565cb749223414b5823e3edc13ac9f41d2fa4
Yoav <yoav.meron11@gmail.com>
2021-07-26 18:12:04 +0300

    changed format of data being returned

--- flask_app/Resources/news.py
+++ flask_app/Resources/news.py
@@ -1,19 +1,16 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from ..Models.news import NewsModel
 
 
 class News(Resource):
-    def __init__(self):
-        nlp_file = ""
-        nlp_table_name = ""
-        conn = NewsModel.create_connection(nlp_file)
-        nlp_dict = NewsModel.select_all_rows(conn, nlp_table_name)
-
-        webscraper_conn = NewsModel.create_connection("news_texts.db")
-        self.webscraper_dict = NewsModel.select_all_rows(conn, webscraper_conn)
-
-        for key, value in self.webscraper_dict.items():
-            value.append(nlp_dict[key])
+    parser = reqparse.RequestParser()
+    parser.add_argument('file_name',
+                        type=str)
+    parser.add_argument('table_name',
+                        type=str)
 
     def get(self):
-        return self.webscraper_dict
+        data = News.parser.parse_args()
+        conn = NewsModel.create_connection(data['file_name'])
+        rows_list = NewsModel.select_all_rows(conn, data['table_name'])
+        return rows_list
