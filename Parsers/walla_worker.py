43c8a0ba2217f43442a2f5492c068c15498f53c0
Yoav <yoav.meron11@gmail.com>
2021-07-26 12:59:53 +0300

    changed topic to fit ynet base topics

--- Parsers/walla_worker.py
+++ Parsers/walla_worker.py
@@ -14,7 +14,20 @@ class WallaParser(BasicParser):
     def topic_parse(self):
         soup = BeautifulSoup(self.page_html, "html.parser")
         nav_ul = soup.find("article", {"class": "common-item"}).find("nav").find("ul")
+        if nav_ul.find_all("li")[-2].a['title'] == "חדשות בעולם":
+            topic = 'חדשות בעולם'
+            return topic
         topic = nav_ul.find_all("li")[-1].a['title']
+        if topic == 'קורונה':
+            topic = "חינוך ובריאות"
+        elif topic == 'פוליטי-מדיני':
+            topic = 'מדיני'
+        elif topic == 'יחסי חוץ':
+            topic = 'מדיני'
+        elif topic == 'חדשות פלילים ומשפט':
+            topic = 'משפט ופלילים'
+        elif topic == 'אירועים בארץ':
+            topic = 'כללי'
         return topic
 
 
