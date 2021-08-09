34a4d68523e0bd0a6571b716b6239299cb310c2f
Yoav <yoav.meron11@gmail.com>
2021-08-09 17:25:58 +0300

    Changed db publisher to be generic for morphology queue

--- DatabaseHandlers/queue_publisher.py
+++ DatabaseHandlers/queue_publisher.py
@@ -2,43 +2,38 @@ import pika
 import json
 
 
-class DatabasePublisher:
-    def __init__(self):
+class QueuePublisher:
+    def __init__(self, queue_name):
         connection_parameter = 'localhost'
+        self.queue_name = queue_name
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=connection_parameter)
         )
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange='database', exchange_type='direct', durable=True)
+        self.channel.queue_declare(queue=self.queue_name, durable=True)
 
-    def insert_data_to_DB_queue(self, newspaper, url, full_text, topic):
+    def insert_data_to_queue(self, newspaper, url, full_text, topic):
         payload = json.dumps([newspaper, url, full_text, topic])
 
-        index = 0
-        routing_key = "routing_" + str(index)
         self.channel.basic_publish(
-            exchange='database',
-            routing_key=routing_key,
+            exchange='',
+            routing_key=self.queue_name,
             body=payload
         )
 
     def notify_handler_of_error(self):
         notification = json.dumps("Error in Parsing.")
-        index = 0
-        routing_key = "routing_" + str(index)
         self.channel.basic_publish(
-            exchange='database',
-            routing_key=routing_key,
+            exchange='',
+            routing_key=self.queue_name,
             body=notification
         )
 
     def send_article_amount(self, amount):
         amount_json = json.dumps(amount)
-        index = 0
-        routing_key = "routing_" + str(index)
         self.channel.basic_publish(
-            exchange='database',
-            routing_key=routing_key,
+            exchange='',
+            routing_key=self.queue_name,
             body=amount_json
         )
         # print("\n [x] Sent {} article to queue".format(newspaper))
