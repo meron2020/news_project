23de0dfa726d822bb1abcbbadaeb83b69ddc5094
Yoav <yoav.meron11@gmail.com>
2021-08-11 16:48:51 +0300

    Created Testing Orchestrator

---
+++ Testing Directory/testing_orchestrator.py
@@ -0,0 +1,22 @@
+import threading
+
+from HebrewMorphologyEngine.morphology_workers_orchestrator import MorphologyWorkersOrchestrator
+from QueueWorkers.workers_orchestrator import WorkersOrchestrator
+from DatabaseHandlers.database_handler_orchestrator import DatabaseHandlerOrchestrator
+from testing_urls_file import TestingUrlsSender
+
+
+class TestingOrchestrator:
+    def run_orchestrator(self):
+        handler = DatabaseHandlerOrchestrator()
+        handler_thread = threading.Thread(target=handler.run_orchestrator())
+        handler_thread.start()
+        cache_thread = threading.Thread(target=handler.create_cache_db())
+        morphology_workers = MorphologyWorkersOrchestrator()
+        morphology_workers.run_orchestrator()
+        workers = WorkersOrchestrator()
+        workers.run_orchestrator()
+        TestingUrlsSender.send_urls()
+
+
+orchestrator = TestingOrchestrator()
