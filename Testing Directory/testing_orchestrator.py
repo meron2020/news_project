b204f04c03f655caedbc2ee2eb0bb277afda97d9
Yoav <yoav.meron11@gmail.com>
2021-08-11 17:27:26 +0300

    Added cache thread

--- Testing Directory/testing_orchestrator.py
+++ Testing Directory/testing_orchestrator.py
@@ -12,6 +12,7 @@ class TestingOrchestrator:
         handler_thread = threading.Thread(target=handler.run_orchestrator())
         handler_thread.start()
         cache_thread = threading.Thread(target=handler.create_cache_db())
+        cache_thread.start()
         morphology_workers = MorphologyWorkersOrchestrator()
         morphology_workers.run_orchestrator()
         workers = WorkersOrchestrator()
