277593a9df56133f559996e141a829e450bcd23f
Yoav <yoav.meron11@gmail.com>
2021-08-12 21:14:31 +0300

    Fixed bug

--- Parsers/ynet_parser.py
+++ Parsers/ynet_parser.py
@@ -12,6 +12,5 @@ class YnetParser(BasicParser):
         texts_span = soup.find_all("span", {"data-text": "true"})
         for span in texts_span:
             texts.append(span.getText())
-        texts.pop(-1)
         full_text = ''.join(texts)
         return full_text
