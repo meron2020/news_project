e7d6ba5958f1b1e3f9ea039724181a9c1b08fbe8
Yoav <yoav.meron11@gmail.com>
2021-08-15 12:48:29 +0300

    Added functionality for prepositions.

--- HebrewMorphologyEngine/morphology_engine.py
+++ HebrewMorphologyEngine/morphology_engine.py
@@ -23,11 +23,23 @@ class HebrewMorphologyEngine:
 
         split_response = response.json()['md_lattice'].split("\n")
         hebrew_words_dict = {}
+        one_word_before = False
         for category in split_response:
             word_list = category.split()
             try:
                 if len(word_list[2]) != 1:
-                    hebrew_words_dict[word_list[2]] = word_list[3]
+                    if one_word_before:
+                        before_word = split_response[split_response.index(category) - 1].split()[2]
+                        two_before_word = split_response[split_response.index(category) - 2].split()[2]
+                        if len(two_before_word) != 1:
+                            hebrew_words_dict[two_before_word + before_word + word_list[2]] = word_list[3]
+                        else:
+                            hebrew_words_dict[before_word + word_list[2]] = word_list[3]
+                        one_word_before = False
+                    else:
+                        hebrew_words_dict[word_list[2]] = word_list[3]
+                else:
+                    one_word_before = True
             except Exception:
                 pass
         return hebrew_words_dict
