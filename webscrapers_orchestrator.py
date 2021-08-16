b75b89f106e6cfc38951f73321d3fe411ac7d6eb
Yoav <yoav.meron11@gmail.com>
2021-08-16 22:47:43 +0300

    Changed worker amounts to 1 for testing and added word dict.

--- webscrapers_orchestrator.py
+++ webscrapers_orchestrator.py
@@ -8,14 +8,15 @@ from HebrewMorphologyEngine.morphology_workers_orchestrator import MorphologyWor
 class WebscrapersOrchestrator:
     def run_orchestrator(self):
         handler = DatabaseHandlerOrchestrator()
+        word_dict = handler.get_all_rows_from_cache()
         handler_thread = threading.Thread(target=handler.run_orchestrator, args=())
         handler_thread.start()
         cache_thread = threading.Thread(target=handler.create_cache_db)
         cache_thread.start()
         morphology_workers = MorphologyWorkersOrchestrator()
-        morphology_workers.run_orchestrator(10)
+        morphology_workers.run_orchestrator(1, word_dict)
         workers = WorkersOrchestrator()
-        workers.run_orchestrator(10)
+        workers.run_orchestrator(1)
 
         crawler_handler = CrawlersHandler()
         crawler_thread = threading.Thread(target=crawler_handler.crawl_links, args=())
