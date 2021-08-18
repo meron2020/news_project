ecd94969a06e9e0172e8facd9cc0673593025c18
Yoav <yoav.meron11@gmail.com>
2021-08-18 12:26:48 +0300

    Created backend orchestrator for webscrapers and graphs.

---
+++ backend_orchestrator.py
@@ -0,0 +1,12 @@
+import threading
+
+from webscrapers_orchestrator import WebscrapersOrchestrator
+from Graphs.networkx_graph import GraphConnections
+
+
+webscrapers = WebscrapersOrchestrator()
+graph_connections = GraphConnections()
+graph_thread = threading.Thread(target=graph_connections.start_consumption, args=())
+graph_thread.start()
+
+webscrapers.run_orchestrator()
