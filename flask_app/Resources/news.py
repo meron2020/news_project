1b8881b055cd47eff43a3c7fd4a3d651e57c2a37
Yoav <yoav.meron11@gmail.com>
2021-08-18 15:12:21 +0300

    Deleted unnecessary parsing and functionality.

--- flask_app/Resources/news.py
+++ flask_app/Resources/news.py
@@ -1,16 +1,11 @@
 from flask_restful import Resource, reqparse
-from ..Models.news import NewsModel
+from flask_app.Models.news import NewsModel
 
 
 class News(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('file_name',
-                        type=str)
-    parser.add_argument('table_name',
-                        type=str)
 
     def get(self):
-        data = News.parser.parse_args()
-        conn = NewsModel.create_connection(data['file_name'])
-        rows_list = NewsModel.select_all_rows(conn, data['table_name'])
+        # conn = NewsModel.create_connection()
+        rows_list = NewsModel.select_all_rows()
         return rows_list
