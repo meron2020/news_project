a8fdd741d95d552677fef844f5dad0277713c66f
Yoav <yoav.meron11@gmail.com>
2021-08-20 17:32:49 +0300

    Added cache thread and handler

--- flask_app/Backend/webscrapers_orchestrator.py
+++ flask_app/Backend/webscrapers_orchestrator.py
@@ -1,18 +1,18 @@
 from flask_app.Backend.QueueWorkers.workers_orchestrator import WorkersOrchestrator
 from flask_app.Backend.News_Crawlers.CrawlersHandler import CrawlersHandler
 import threading
-from DatabaseHandlers.database_handler_orchestrator import DatabaseHandlerOrchestrator
+from flask_app.Backend.DatabaseHandlers.database_handler_orchestrator import DatabaseHandlerOrchestrator
 from flask_app.Backend.HebrewMorphologyEngine.morphology_workers_orchestrator import MorphologyWorkersOrchestrator
 
 
 class WebscrapersOrchestrator:
     def run_orchestrator(self):
         handler = DatabaseHandlerOrchestrator()
+        cache_thread = threading.Thread(target=handler.create_cache_db)
+        cache_thread.start()
         word_dict = handler.get_all_rows_from_cache()
         handler_thread = threading.Thread(target=handler.run_orchestrator, args=())
         handler_thread.start()
-        cache_thread = threading.Thread(target=handler.create_cache_db)
-        cache_thread.start()
         morphology_workers = MorphologyWorkersOrchestrator()
         morphology_workers.run_orchestrator(3, word_dict)
         workers = WorkersOrchestrator()
