0b8a00af019a1dbae4c9684cda351933904a8dde
Yoav <yoav.meron11@gmail.com>
2021-08-22 16:53:04 +0300

    Created sidebar.

---
+++ frontend/src/Components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Row, Col} from 'reactstrap';
+import "../App.css"
+import {SidebarData} from "./SidebarData";
+
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+            <ul className="SidebarList">
+                {SidebarData.map((val, key) => {
+                    return <li key={key} className="row"
+                               id={window.location.pathname === val.link ? "active": ""} onClick={() => {
+                        window.location.pathname = val.link
+                    }}>
+                        <Col className="text-center">
+                            {val.icon} {val.title}
+                        </Col>
+                    </li>
+
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
