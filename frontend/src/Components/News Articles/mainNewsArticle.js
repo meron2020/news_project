005afe1713ef61ec839da88e5e98dc42de3b0b12
Yoav <yoav.meron11@gmail.com>
2021-08-23 14:54:07 +0300

    Copied news article to keep differences between main article and other articles.

---
+++ frontend/src/Components/News Articles/mainNewsArticle.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './newsArticle.css'
+
+
+class MainNewsArticle extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="NewsArticleWithLink">
+                <a href={this.props.article["url"]}>
+                    <div className="NewsArticle">
+                        <article>
+                            <h3>{this.props.article["title"]}</h3>
+                            <h6>{this.props.article["newspaper"]}</h6>
+                        </article>
+                    </div>
+                </a>
+            </div>
+        )
+    }
+}
+
+export default MainNewsArticle;
