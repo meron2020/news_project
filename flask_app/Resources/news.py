c844e3fdb98631dacec6ab8137e1e73109206c4b
Yoav <yoav.meron11@gmail.com>
2021-08-23 14:54:56 +0300

    Added functionality for each topic in get function.

--- flask_app/Resources/news.py
+++ flask_app/Resources/news.py
@@ -5,7 +5,22 @@ from flask_app.Models.news import NewsModel
 class News(Resource):
     parser = reqparse.RequestParser()
 
-    def get(self):
-        # conn = NewsModel.create_connection()
-        rows_list = NewsModel.select_all_rows()
-        return rows_list
+    def __init__(self):
+        self.topic_dict = {"military": 'צבא וביטחון',
+                           "state": 'מדיני',
+                           "politics": 'המערכת הפוליטית',
+                           "palestine": 'פלסטינים',
+                           "general": 'כללי',
+                           "law": 'משפט ופלילים',
+                           "health and education": 'חינוך ובריאות',
+                           "world": 'חדשות בעולם'}
+
+    def get(self, topic):
+        rows_list = []
+        if topic == "state and politics":
+            rows_list.extend(NewsModel.select_all_topic_rows(self.topic_dict["state"]))
+            rows_list.extend(NewsModel.select_all_topic_rows(self.topic_dict["politics"]))
+        else:
+            topic = self.topic_dict[topic]
+            rows_list = NewsModel.select_all_topic_rows(topic)
+        return {"rows_list": rows_list}
