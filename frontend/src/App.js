63e0163873f13c25325b1fbe65cf0d981bf601c3
Yoav <yoav.meron11@gmail.com>
2021-08-24 17:05:15 +0300

    Changed topic titles to hebrew.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,15 +17,17 @@ class App extends React.Component {
         return (<div className="App">
             <>
                 <Router>
+                    <div className="NavBar">
                     <Navbar/>
+                    </div>
                     <Switch>
-                        <Route path="/military" component={() => <TopicPage topicTitle="Military" topicName="military"/>}/>
-                        <Route path="/general" component={() => <TopicPage topicTitle="General" topicName="general"/>}/>
-                        <Route path="/law" component={() => <TopicPage topicTitle="Law" topicName="law"/>}/>
-                        <Route path="/palestine" component={() => <TopicPage topicTitle="Palestine" topicName="palestine"/>}/>
-                        <Route path="/world" component={() => <TopicPage topicTitle="World" topicName="world"/>}/>
-                        <Route path="/state_and_politics" component={() => <TopicPage topicTitle="State and Politics" topicName="state and politics"/>}/>
-                        <Route path="/health_and_education" component={() => <TopicPage topicTitle="Health and Education" topicName="health and education"/>}/>
+                        <Route path="/military" component={() => <TopicPage topicTitle="צבא וביטחון" topicName="military"/>}/>
+                        <Route path="/general" component={() => <TopicPage topicTitle="כללי" topicName="general"/>}/>
+                        <Route path="/law" component={() => <TopicPage topicTitle="משפט ופלילים" topicName="law"/>}/>
+                        <Route path="/palestine" component={() => <TopicPage topicTitle="פלסטינים" topicName="palestine"/>}/>
+                        <Route path="/world" component={() => <TopicPage topicTitle="חדשות בעולם" topicName="world"/>}/>
+                        <Route path="/state_and_politics" component={() => <TopicPage topicTitle="מדיני-פוליטי" topicName="state and politics"/>}/>
+                        <Route path="/health_and_education" component={() => <TopicPage topicTitle="בריאות וחינוך" topicName="health and education"/>}/>
                     </Switch>
                 </Router>
             </>
