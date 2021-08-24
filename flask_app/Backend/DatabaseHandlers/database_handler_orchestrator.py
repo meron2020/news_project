7a4df181627590e6810473adbcc531b7e24111d0
Yoav <yoav.meron11@gmail.com>
2021-08-24 12:21:24 +0300

    Added morphed_title column to articles table.

--- flask_app/Backend/DatabaseHandlers/database_handler_orchestrator.py
+++ flask_app/Backend/DatabaseHandlers/database_handler_orchestrator.py
@@ -12,7 +12,7 @@ class DatabaseHandlerOrchestrator:
         print("Successfully Connected to articles DB Table")
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, newspaper TEXT,"
-            "full_text TEXT, topic Text, title Text, cluster_id Text);")
+            "full_text TEXT, topic Text, title Text, morphed_title Text, cluster_id Text);")
 
         handler.delete_all_rows()
         print("Successfully deleted all rows in articles table")
@@ -47,6 +47,14 @@ class DatabaseHandlerOrchestrator:
         return handler.return_word_to_morph_dict()
 
     def get_all_rows(self):
+        connection = sqlite3.connect(r"../../news_texts.db")
+        cursor = connection.cursor()
+        table_name = "articles"
+        handler = DatabaseHandler(connection, cursor, table_name)
+        print("Successfully Connected to SQLite")
+        return handler.select_all_rows()
+
+    def get_all_rows_for_nlp(self):
         connection = sqlite3.connect(r"../../news_texts.db")
         cursor = connection.cursor()
         table_name = "articles"
