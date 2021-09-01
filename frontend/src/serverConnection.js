1a4cbd00247cf532887ac1465451bb6162f94896
Yoav <yoav.meron11@gmail.com>
2021-09-01 16:24:51 +0300

    Added get score logs function.

--- frontend/src/serverConnection.js
+++ frontend/src/serverConnection.js
@@ -4,19 +4,32 @@ const fetch = require('node-fetch')
 const ServerConnection = {
     url: "http://127.0.0.1:5000/",
 
+    async getScoreLogs() {
+        try {
+            const response = await fetch(`${ServerConnection.url}score_logs/`,
+                {headers: {"connection": "keep-alive"}})
+            if (response.ok) {
+                let responseJson = await response.json();
+                return responseJson["rows_list"];
+            }
+            throw new Error("Request failed!");
+        } catch (error) {
+            document.write(error)
+        }
+    },
+
     async getNews(topic) {
         try {
             const response = await fetch(`${ServerConnection.url}news/${topic}`,
                 {headers: {"connection": "keep-alive"}})
             if (response.ok) {
-                let responseJson =  await response.json();
+                let responseJson = await response.json();
                 return responseJson["rows_list"];
             }
             throw new Error("Request failed!");
-        }
-        catch(error) {
+        } catch (error) {
             document.write(error)
         }
-        }
+    }
 }
 export default ServerConnection
