4b3bd5a08c97abf1aad656d8ad5002085e18c717
Yoav <yoav.meron11@gmail.com>
2021-09-01 16:11:24 +0300

    reformatted.

--- flask_app/Backend/backend_orchestrator.py
+++ flask_app/Backend/backend_orchestrator.py
@@ -1,9 +1,6 @@
 import threading
-
 from webscrapers_orchestrator import WebscrapersOrchestrator
 from flask_app.Backend.Graphs.networkx_graph import GraphConnections
-
-
 webscrapers = WebscrapersOrchestrator()
 graph_connections = GraphConnections()
 graph_thread = threading.Thread(target=graph_connections.start_consumption, args=())
