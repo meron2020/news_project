c38a60ba863f3348e4d3abd8d316e02abbfcf293
Yoav <yoav.meron11@gmail.com>
2021-09-01 16:19:50 +0300

    Added all functions related to score storing.

--- flask_app/Backend/DatabaseHandlers/database_handler_orchestrator.py
+++ flask_app/Backend/DatabaseHandlers/database_handler_orchestrator.py
@@ -19,8 +19,25 @@ class DatabaseHandlerOrchestrator:
 
         handler.start_consumption()
 
-    def update_cluster_ids(self, cluster_ids_dict):
+    def create_score_db(self):
         connection = sqlite3.connect(r"../../news_texts.db")
+        cursor = connection.cursor()
+        table_name = "scores"
+        handler = DatabaseHandler(connection, cursor, table_name)
+        print("Successfully Connected to scores DB Table")
+        cursor.execute(
+            "DROP TABLE {}".format(table_name)
+        )
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS scores "
+            "(first_id Int, second_id Int, "
+            "first_title Text, second_title Text, title_score Int, text_score Int, total_score Int);")
+
+        handler.delete_all_score_rows()
+        print("Successfully deleted all rows in scores table")
+
+    def update_cluster_ids(self, cluster_ids_dict, path):
+        connection = sqlite3.connect(path)
         cursor = connection.cursor()
         table_name = "articles"
         handler = DatabaseHandler(connection, cursor, table_name)
@@ -54,17 +71,70 @@ class DatabaseHandlerOrchestrator:
         print("Successfully Connected to SQLite")
         return handler.select_all_rows()
 
-    def get_all_rows_for_nlp(self):
-        connection = sqlite3.connect(r"../../news_texts.db")
+    def get_all_rows_for_graph(self, path):
+        connection = sqlite3.connect(path)
+        cursor = connection.cursor()
+        table_name = "articles"
+        handler = DatabaseHandler(connection, cursor, table_name)
+        print("Successfully Connected to SQLite")
+        rows = handler.select_all_rows()
+        rows_dict = {}
+        for row in rows:
+            rows_dict[row[0]] = row
+        return rows_dict
+
+    def get_all_rows_from_scores(self, path):
+        connection = sqlite3.connect(path)
+        cursor = connection.cursor()
+        table_name = "scores"
+        handler = DatabaseHandler(connection, cursor, table_name)
+        print("Successfully Connected to SQLite")
+        return handler.select_all_rows()
+
+    def get_all_rows_for_nlp(self, path):
+        connection = sqlite3.connect(path)
         cursor = connection.cursor()
         table_name = "articles"
         handler = DatabaseHandler(connection, cursor, table_name)
         print("Successfully Connected to SQLite")
         return handler.select_all_rows()
 
-    def get_url_by_id(self, _id):
+    def insert_scores(self, first_id, second_id, first_title, second_title, title_score, text_score, total_score, path):
+        connection = sqlite3.connect(path)
+        cursor = connection.cursor()
+        table_name = "scores"
+        handler = DatabaseHandler(connection, cursor, table_name)
+        handler.insert_article_scores(first_id, second_id, first_title, second_title, title_score, text_score,
+                                      total_score)
+
+    def delete_all_rows_in_scores(self):
         connection = sqlite3.connect(r"../../news_texts.db")
+        cursor = connection.cursor()
+        table_name = "scores"
+        handler = DatabaseHandler(connection, cursor, table_name)
+        handler.delete_all_score_rows()
+
+    def get_url_by_id(self, _id, path):
+        connection = sqlite3.connect(path)
         cursor = connection.cursor()
         table_name = "articles"
         handler = DatabaseHandler(connection, cursor, table_name)
         return handler.get_url_by_id(_id)
+
+    def get_url_to_url_score(self, path):
+        url_to_url_score = []
+        for score_row in self.get_all_rows_from_scores(path):
+            first_url = self.get_url_by_id(score_row[0], path)
+            second_url = self.get_url_by_id(score_row[1], path)
+            first_title = self.get_title_by_id(score_row[0], path)
+            second_title = self.get_title_by_id(score_row[1], path)
+            url_to_url_score.append(
+                [first_url, second_url, first_title, second_title, score_row[2], score_row[3], score_row[4]])
+        return url_to_url_score
+
+    def get_title_by_id(self, _id, path):
+        connection = sqlite3.connect(path)
+        cursor = connection.cursor()
+        table_name = "articles"
+        handler = DatabaseHandler(connection, cursor, table_name)
+        return handler.get_title_by_id(_id)
