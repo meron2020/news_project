67977f07651eb9cde44d1e50d7982cbb42425745
Yoav <yoav.meron11@gmail.com>
2021-09-01 21:38:30 +0300

    Trying to fix bug in heroku.

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api
-from flask_jwt import JWT
-from datetime import timedelta
-from flask_app.db import db
 from flask_app.Resources.news import News
 from flask_app.Resources.score_logs import ScoreLogs
 from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(minutes=15)
-
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'yoav'
+CORS(app)
 api = Api(app)
-
-api.add_resource(News, '/news/<string:topic>')
-api.add_resource(ScoreLogs, '/score_logs/')
 
 
 @app.route('/')
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
+
+api.add_resource(News, '/news/<string:topic>')
+api.add_resource(ScoreLogs, '/score_logs/')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
