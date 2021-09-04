fa746090af153f299ab87801b41759736ef3ad22
Yoav <yoav.meron11@gmail.com>
2021-09-04 13:44:30 +0300

    turned backend orchestrator into a class.

--- flask_app/Backend/backend_orchestrator.py
+++ flask_app/Backend/backend_orchestrator.py
@@ -1,9 +1,19 @@
 import threading
 from webscrapers_orchestrator import WebscrapersOrchestrator
 from flask_app.Backend.Graphs.networkx_graph import GraphConnections
-webscrapers = WebscrapersOrchestrator()
-graph_connections = GraphConnections()
-graph_thread = threading.Thread(target=graph_connections.start_consumption, args=())
-graph_thread.start()
 
-webscrapers.run_orchestrator()
+
+class BackendOrchestrator:
+    def __init__(self):
+        self.webscrapers = WebscrapersOrchestrator()
+        self.graph_connections = GraphConnections()
+
+    def run_orchestrator(self):
+        graph_thread = threading.Thread(target=self.graph_connections.start_consumption, args=())
+        graph_thread.start()
+
+        self.webscrapers.run_orchestrator()
+
+
+orchestrator = BackendOrchestrator()
+orchestrator.run_orchestrator()
