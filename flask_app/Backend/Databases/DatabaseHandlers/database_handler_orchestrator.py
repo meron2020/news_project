bcaa00fa3b9d317cf7234a8e749383c90c4d7f53
Yoav <yoav.meron11@gmail.com>
2021-09-06 18:11:26 +0300

    Turned all methods into instance methods rather than class methods.

--- flask_app/Backend/Databases/DatabaseHandlers/database_handler_orchestrator.py
+++ flask_app/Backend/Databases/DatabaseHandlers/database_handler_orchestrator.py
@@ -4,71 +4,60 @@ from flask_app.Backend.Databases.DatabaseHandlers.cache_database_handler import 
 
 
 class DatabaseHandlerOrchestrator:
+    def __init__(self):
+        self.handler = DatabaseHandler()
+        self.cache_handler = CacheDatabaseHandler()
+
     def run_orchestrator(self):
-        handler = DatabaseHandler()
-        print("Successfully Connected to articles DB Table")
-        handler.delete_all_rows()
+        self.handler.delete_all_rows()
         print("Successfully deleted all rows in articles table")
-        handler.start_consumption()
+        self.handler.start_consumption()
 
     def create_score_db(self):
-        handler = DatabaseHandler()
-        print("Successfully Connected to scores DB Table")
         try:
-            handler.delete_all_score_rows()
+            self.handler.delete_all_score_rows()
         except sqlite3.OperationalError:
             pass
-        handler.delete_all_score_rows()
+        self.handler.delete_all_score_rows()
         print("Successfully deleted all rows in scores table")
 
     def update_cluster_ids(self, cluster_ids_dict):
-        handler = DatabaseHandler()
         for cluster_id, articles in cluster_ids_dict.items():
             for article in articles:
-                handler.update_cluster_id(article, cluster_id)
+                self.handler.update_cluster_id(article, cluster_id)
 
     def create_cache_db(self):
-        handler = CacheDatabaseHandler()
         print("Successfully Connected to morph_cache DB Table")
-        handler.start_consumption()
+        self.cache_handler.start_consumption()
 
     def get_all_rows_from_cache(self):
-        handler = CacheDatabaseHandler()
-        return handler.return_word_to_morph_dict()
+        return self.cache_handler.return_word_to_morph_dict()
 
     def get_all_rows(self):
-        handler = DatabaseHandler()
         print("Successfully Connected to SQLite")
-        return handler.select_all_articles()
+        return self.handler.select_all_articles()
 
-    def get_all_rows_for_graph(self, path):
-        handler = DatabaseHandler()
+    def get_all_rows_for_graph(self):
         print("Successfully Connected to SQLite")
-        rows = handler.select_all_articles()
+        rows = self.handler.select_all_articles()
         rows_dict = {}
         for row in rows:
             rows_dict[row[0]] = row
         return rows_dict
 
     def get_all_rows_from_scores(self):
-        handler = DatabaseHandler()
-        print("Successfully Connected to SQLite")
-        return handler.select_all_scores()
+        return self.handler.select_all_scores()
 
     def get_all_rows_for_nlp(self):
-        handler = DatabaseHandler()
         print("Successfully Connected to SQLite")
-        return handler.select_all_articles()
+        return self.handler.select_all_articles()
 
     def insert_scores(self, first_id, second_id, first_title, second_title, title_score, text_score, total_score, path):
-        handler = DatabaseHandler()
-        handler.insert_article_scores(first_id, second_id, first_title, second_title, title_score, text_score,
-                                      total_score)
+        self.handler.insert_article_scores(first_id, second_id, first_title, second_title, title_score, text_score,
+                                           total_score)
 
     def delete_all_rows_in_scores(self):
-        handler = DatabaseHandler()
-        handler.delete_all_score_rows()
+        self.handler.delete_all_score_rows()
 
     def get_url_by_id(self, _id):
-        handler = DatabaseHandler()
-        return handler.get_url_by_id(_id)
+        return self.handler.get_url_by_id(_id)
